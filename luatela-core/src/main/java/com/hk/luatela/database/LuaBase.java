cbbcc5d726bcd0452cba1740476e85643b0e61e3
thekayani <kayanihashim@gmail.com>
2021-10-05 10:50:12 -0400

    begin model patch work

--- luatela-core/src/main/java/com/hk/luatela/database/LuaBase.java
+++ luatela-core/src/main/java/com/hk/luatela/database/LuaBase.java
@@ -1,21 +1,56 @@
 package com.hk.luatela.database;
 
+import com.hk.util.Requirements;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
+import java.util.Objects;
 
 public class LuaBase
 {
+	private final File dataroot;
 	private Connection connection;
-	private final File dataroot, patches;
 
-	public LuaBase(File dataroot)
+	public LuaBase(File dataroot) throws FileNotFoundException
 	{
+		if(!dataroot.exists())
+			throw new FileNotFoundException(dataroot.getAbsolutePath());
+
 		this.dataroot = dataroot;
-		patches = new File(dataroot, ".patches");
 	}
 
 	public void setConnection(Connection connection)
 	{
 		this.connection = connection;
+	}
+
+	public void checkPatches()
+	{
+		File patchesDir = new File(dataroot, ".patches");
+
+		if(!patchesDir.exists())
+		{
+			patchesDir.mkdirs();
+			return;
+		}
+
+		File[] patches = patchesDir.listFiles();
+
+		if(patches != null)
+		{
+			for(File patch : patches)
+				System.out.println("Patch exists: " + patch);
+		}
+	}
+
+	public void checkNew() throws FileNotFoundException
+	{
+		File models = new File(dataroot, "models.lua");
+
+		if(!models.exists())
+			throw new FileNotFoundException(models.getAbsolutePath() + " (models.lua required for db)");
+
+
 	}
 }
