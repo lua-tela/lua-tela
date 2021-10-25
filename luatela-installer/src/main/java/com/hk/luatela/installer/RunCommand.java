5ad7c9ce681fcb3bb096ea9462350791c0c135d4
theKayani <kayanihashim@gmail.com>
2021-10-24 23:33:20 -0400

    SSL/HTTPS installer support

--- luatela-installer/src/main/java/com/hk/luatela/installer/RunCommand.java
+++ luatela-installer/src/main/java/com/hk/luatela/installer/RunCommand.java
@@ -3,18 +3,21 @@ package com.hk.luatela.installer;
 import com.hk.file.FileUtil;
 import com.hk.io.IOUtil;
 import com.hk.str.HTMLText;
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.SecuredRedirectHandler;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -99,18 +102,88 @@ class RunCommand extends Installer.Command
 					f.renameTo(new File(libDir, name + ".tmp"));
 			}
 
+			int httpPort = 8080;
+			int httpsPort = 8443;
+			boolean sslEnabled = false;
+
+			String httpPortParam = Installer.getParam(arguments, "--httpPort");
+			if(httpPortParam != null)
+				httpPort = Integer.parseInt(httpPortParam);
+
+			Resource keystoreResource = null;
+			String keystorePath = Installer.getParam(arguments, "--keystore");
+			String keystorePass = null, keyManagerPass = null;
+			if(keystorePath != null)
+			{
+				String httpsPortParam = Installer.getParam(arguments, "--httpsPort");
+				if(httpsPortParam != null)
+					httpsPort = Integer.parseInt(httpsPortParam);
+
+				keystoreResource = Resource.newResource(keystorePath);
+
+				keystorePass = Installer.getParam(arguments, "--keystorePass");
+				if(keystorePass == null)
+					throw new IllegalStateException("Expected 'keystorePass' param alongside keystore");
+
+				keyManagerPass = Installer.getParam(arguments, "--keyManagerPass");
+
+				if(keyManagerPass == null)
+					keyManagerPass = keystorePass;
+
+				sslEnabled = true;
+			}
+
+			if(!arguments.isEmpty())
+				System.err.println("\nUnexpected command line parameter(s): " + arguments + "\n");
+
 			this.server = new Server();
 
-			ServerConnector connector = new ServerConnector(server);
-			connector.setPort(8080);
-			server.setConnectors(new Connector[] { connector });
+			if(sslEnabled)
+			{
+				HttpConfiguration https = new HttpConfiguration();
+				https.setSecureScheme("https");
+				https.setSecurePort(httpsPort);
+				https.addCustomizer(new SecureRequestCustomizer());
+
+				ServerConnector httpConnector = new ServerConnector(server,
+						new HttpConnectionFactory(https));
+				httpConnector.setPort(httpPort);
+
+				SslContextFactory sslContextFactory = new SslContextFactory.Server.Server();
+				sslContextFactory.setKeyStoreResource(keystoreResource);
+				sslContextFactory.setKeyStorePassword(keystorePass);
+				sslContextFactory.setKeyManagerPassword(keyManagerPass);
+
+				ServerConnector httpsConnector = new ServerConnector(server,
+						new SslConnectionFactory(sslContextFactory, "http/1.1"),
+						new HttpConnectionFactory(https));
+				httpsConnector.setPort(httpsPort);
+
+				server.setConnectors(new Connector[] { httpConnector, httpsConnector });
+			}
+			else
+			{
+				ServerConnector httpConnector = new ServerConnector(server);
+				httpConnector.setPort(httpPort);
+
+				server.setConnectors(new Connector[] { httpConnector });
+			}
 
 			WebAppContext context = new WebAppContext(tempDir.toString(), "/");
 
-			server.setHandler(context);
+			if(sslEnabled)
+			{
+				HandlerList list = new HandlerList();
+				list.addHandler(new SecuredRedirectHandler());
+				list.addHandler(context);
+
+				server.setHandler(list);
+			}
+			else
+				server.setHandler(context);
 
 			server.start();
-
+			
 			while(true)
 			{
 				line = in.nextLine();
@@ -156,16 +229,17 @@ class RunCommand extends Installer.Command
 
 		String str;
 
+		txt.prln("--dataroot [data root directory]").tabUp();
 		str = "This parameter should point to the 'dataroot' " +
 				"directory, which contains the route and model info.\n" +
 				"By default, if not provided, it will point to the " +
 				"current working directory. This folder important and " +
 				"required for Lua Tela to properly initialize.";
-		txt.prln("--dataroot [data root directory]").tabUp();
 		for(String line : splitToLinesByLen(str, 50))
 			txt.prln(line);
 		txt.tabDown();
 
+		txt.prln("--resourceroot [resource root directory]").tabUp();
 		str = "This parameter should point to the 'resourceroot' " +
 				"directory. This is the default directory in which " +
 				"Lua Tela will search to serve static resource files, " +
@@ -174,11 +248,11 @@ class RunCommand extends Installer.Command
 				"initialized using the 'dataroot' and 'resourcepath' " +
 				"parameter. It will be initialized as: " +
 				"'dataroot/resourcepath'";
-		txt.prln("--resourceroot [resource root directory]").tabUp();
 		for(String line : splitToLinesByLen(str, 50))
 			txt.prln(line);
 		txt.tabDown();
 
+		txt.prln("--resourcepath [web app resource path]").tabUp();
 		str = "This parameter will be the URL path prefix that should " +
 				"indicate when static files should be served.\n" +
 				"By default, if not provided, this will default to " +
@@ -186,36 +260,77 @@ class RunCommand extends Installer.Command
 				"'http://yoursite/res', Lua Tela will look for the " +
 				"resourceroot directory and find the file relative to " +
 				"the path and serve it with the respective content type.";
-		txt.prln("--resourcepath [web app resource path]").tabUp();
 		for(String line : splitToLinesByLen(str, 50))
 			txt.prln(line);
 		txt.tabDown();
 
+		txt.prln("--httpPort [0-65535 port number]").tabUp();
+		str = "This parameter is the port that the HTTP protocol should " +
+				"listen on. By default, this is set to listen to " +
+				"requests coming in on port 8080.";
+		for(String line : splitToLinesByLen(str, 50))
+			txt.prln(line);
+		txt.tabDown();
+
+		txt.prln("--httpsPort [0-65535 port number]").tabUp();
+		str = "This parameter is the port that the HTTPS protocol should " +
+				"listen on. This will only be used if the 'keystore' " +
+				"parameter is enabled with a valid keystore resource.";
+		for(String line : splitToLinesByLen(str, 50))
+			txt.prln(line);
+		txt.tabDown();
+
+		txt.prln("--keystore [file path or URL to keystore]").tabUp();
+		str = "This parameter should point to a valid JKS file which. " +
+				"will enable the HTTPS port and also redirect HTTP " +
+				"requests to the HTTPS port. Alongside this parameter " +
+				"are also the 'keystorePass' and 'keyManagerPass' " +
+				"parameters which should also be specified.";
+		for(String line : splitToLinesByLen(str, 50))
+			txt.prln(line);
+		txt.tabDown();
+
+		txt.prln("--keystorePass [password for JKS]").tabUp();
+		str = "This parameter will be used to authenticate the " +
+				"Java KeyStore. This parameter is required alongside " +
+				"'keystore' and will only be used if that is present.";
+		for(String line : splitToLinesByLen(str, 50))
+			txt.prln(line);
+		txt.tabDown();
+
+		txt.prln("--keyManagerPass [password for JKS key manager]").tabUp();
+		str = "This parameter should be the key manager password for " +
+				"the specified Java KeyStore using the 'keystore' " +
+				"parameter.";
+		for(String line : splitToLinesByLen(str, 50))
+			txt.prln(line);
+		txt.tabDown();
+
+		txt.prln("--datauser [user for MySQL database]").tabUp();
 		str = "This parameter indicates the database user to login " +
 				"with when creating a connection pool for web requests. " +
 				"The default user for MySQL server is 'root' but that " +
 				"is not preferred as it has access to various databases. " +
 				"You should create a separate 'luatela' user to access " +
 				"databases and tables with Lua Tela.";
-		txt.prln("--datauser [user for MySQL database]").tabUp();
 		for(String line : splitToLinesByLen(str, 50))
 			txt.prln(line);
 		txt.tabDown();
 
+		txt.prln("--datapass [password for respective MySQL user]").tabUp();
 		str = "This parameter is the password for the respective " +
 				"'datauser' when establishing a connection pool for " +
 				"requests.";
-		txt.prln("--datapass [password for respective MySQL user]").tabUp();
 		for(String line : splitToLinesByLen(str, 50))
 			txt.prln(line);
 		txt.tabDown();
 
+		txt.prln("--dataurl [JDBC URL format for MySQL database]").tabUp();
 		str = "This parameter is the JDBC URL format for establishing " +
 				"a connection for a MySQL database. This parameter " +
 				"usually looks something like so...\n" +
 				"jdbc:mysql://mysql.db.server:3306/my_database?useSSL=false&serverTimezone=UTC\n" +
 				"jdbc:mysql://[host-name]:[port]/[database]?[extra-parameters]";
-		txt.prln("--dataurl [JDBC URL format for MySQL database]").tabUp();
 		for(String line : splitToLinesByLen(str, 50))
 			txt.prln(line);
 		txt.tabDown();
